f3d889a0454234079d46ba5ede142ea94f057900
Riday <ridayshah2001@gmail.com>
2020-11-06 12:06:29 +0530

    Feat: home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = () => <div>Home Page</div>;
+const Home = () => {
+	useEffect(() => {
+		document.title = 'Home | Medicare';
+		return undefined;
+	}, []);
+
+	return (
+		<div className='box col-11 col-md-6 mx-auto justify-content-center py-5'>
+			<h2 id='title' className='text-center'>
+				Home
+			</h2>
+
+			<div className='d-flex justify-content-around mt-4 nav'>
+				<Link to={{ pathname: '/login' }}>Login</Link>
+				<Link to={{ pathname: '/register' }}>Register</Link>
+			</div>
+
+			<div id='quotes' className='col-md-8 justify-content-around mx-auto mt-5'>
+				<div className='quote'>
+					“The art of medicine consists of amusing the patient while nature cures the disease.”
+					<div className='d-flex justify-content-end'>― Voltaire</div>
+				</div>
+
+				<div className='quote'>
+					“Vaccines are the most cost-effective health care interventions there are. A dollar spent on a
+					childhood vaccination not only helps save a life, but greatly reduces spending on future
+					healthcare.”
+					<div className='d-flex justify-content-end'>― Ezekiel Emanuel</div>
+				</div>
+
+				<div className='quote'>
+					“Like education, healthcare also needs to be given importance.”
+					<div className='d-flex justify-content-end'>― Shiv Nadar</div>
+				</div>
+
+				<div className='quote'>
+					“The goal of real healthcare reform must be high-quality, universal coverage in a cost-effective
+					way.”
+					<div className='d-flex justify-content-end'>― Bernie Sanders</div>
+				</div>
+
+				<div className='quote'>
+					“Healthcare is the cornerstone of the socialist state. It is the crown jewel of the welfare state.”
+					<div className='d-flex justify-content-end'>― Monica Crowley</div>
+				</div>
+
+				<div className='quote'>
+					“People have to take control of their own lives. Education is key because it also raises other
+					social indicators like healthcare.”
+					<div className='d-flex justify-content-end'>― Azem Premji</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Home;
