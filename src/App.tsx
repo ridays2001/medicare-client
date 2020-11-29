33e7978851dfe381f3e2f9a9243d29ad85d04ad4
Riday <ridayshah2001@gmail.com>
2020-11-29 13:27:26 +0530

    Style: removed useless return statement

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import routes from './routes';
 
-const App = () => {
-	return (
-		<main>
-			<Switch>
-				{routes.map((route) => (
-					<Route {...route} />
-				))}
-			</Switch>
-		</main>
-	);
-};
+const App = () => (
+	<main>
+		<Switch>
+			{routes.map((route) => (
+				<Route {...route} />
+			))}
+		</Switch>
+	</main>
+);
 
 export default App;
