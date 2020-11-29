dab09fc8adc42a25c8de0c850b342ddcae09ef74
Riday <ridayshah2001@gmail.com>
2020-11-29 10:52:50 +0530

    Feat: added cookie provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Link } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 import App from './App';
 
 import { Logo } from './util/icons';
@@ -9,13 +10,15 @@ import './styles/bootstrap.css';
 import './styles/index.css';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<React.StrictMode>
-			<Link to={{ pathname: '/' }}>
-				<Logo />
-			</Link>
-			<App />
-		</React.StrictMode>
-	</BrowserRouter>,
+	<CookiesProvider>
+		<BrowserRouter>
+			<React.StrictMode>
+				<Link to={{ pathname: '/' }}>
+					<Logo />
+				</Link>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
+	</CookiesProvider>,
 	document.getElementById('root')
 );
